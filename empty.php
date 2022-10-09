ad4596d8d74e809fbf1dff63d7309ce56af1fa90
Nolan Trapp <officialstackemup@gmail.com>
2022-10-08 18:59:29 -0700

    Update empty.php

--- empty.php
+++ empty.php
@@ -78,7 +78,7 @@ if ($files) {
 <body>
 <div id="panel">
 
-	<a href="https://api.wuffs.net/?i=<?= $random_fox_index ?>" id="fox_full_link">
+	<a href="https://api.wuffs.net/images/<?= $random_fox_index ?>" id="fox_full_link">
 		<img src="https://api.wuffs.net/images/<?= $random_fox_index ?>.jpg" alt="" title="" style="visibility: hidden;" id="fox_img_link" />
 	</a>
 
