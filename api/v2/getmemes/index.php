5c8be9093087e9709d81276d20f3f795ab0b5cf0
Nolan <officialstackemup@gmail.com>
2022-10-17 11:44:27 -0700

    Update index.php

--- api/v2/getmemes/index.php
+++ api/v2/getmemes/index.php
@@ -7,7 +7,7 @@ $files = glob('../images/*');
 $FOX_NUM = 14;
 $random_fox_index = rand(1, $FOX_NUM);
 
-$image_path = 'https://s.wuffs.net/api'.$random_fox_index.'.jpg';
+$image_path = 'https://s.wuffs.net/api'.$random_fox_index;
 
 #$data = ['image' => $image_path, 'link' => $link];
 $data = ['image' => $image_path];
