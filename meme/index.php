3110162b85760a425beaa923fc7f655a44fed5af
Nolan <officialstackemup@gmail.com>
2022-10-17 12:51:28 -0700

    Update index.php

--- meme/index.php
+++ meme/index.php
@@ -2,14 +2,10 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-$files = glob('../images/*');
-if ($files) {
-    $FOX_NUM = count($files);
-    $random_fox_index = rand(1, $FOX_NUM);
+$FOX_NUM = 14;
+$random_fox_index = rand(1, $FOX_NUM);
 
     $link = 'https://s.wuffs.net/api'.$random_fox_index;
-} else {
-    $link = null;
-}
+
 $data = ['image' => $link, 'version' => '2022.0.4.15'];
 echo json_encode($data);
