d5dcf6f5342791e76d227af6c639b321916837d0
Nolan <officialstackemup@gmail.com>
2022-10-17 11:41:43 -0700

    Create index.php

---
+++ api/v2/getmemes/index.php
@@ -0,0 +1,14 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$files = glob('../images/*');
+
+$FOX_NUM = 14;
+$random_fox_index = rand(1, $FOX_NUM);
+
+$image_path = 'https://s.wuffs.net/api'.$random_fox_index.'.jpg';
+
+#$data = ['image' => $image_path, 'link' => $link];
+$data = ['image' => $image_path];
+echo json_encode($data);
