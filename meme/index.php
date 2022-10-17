5d955efa113bbcaf1e40a1a10c6672ba06a08cca
Nolan <officialstackemup@gmail.com>
2022-10-17 12:57:34 -0700

    hrggt

--- meme/index.php
+++ meme/index.php
@@ -7,5 +7,5 @@ $random_fox_index = rand(1, $FOX_NUM);
 
     $link = 'https://s.wuffs.net/api'.$random_fox_index;
 
-$data = ['image' => $link, 'version' => '2022.0.4.15', 'status' => 'Depreciated'];
+$data = ['image' => $link, 'version' => '2022.0.4.15', 'status' => 'Depreciated', 'msg' => 'Please use V2 of api'];
 echo json_encode($data);
