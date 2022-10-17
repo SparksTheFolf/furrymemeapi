92904eb849b1d03254ffdb39b80f4d722566bdfa
Nolan <officialstackemup@gmail.com>
2022-10-17 12:02:13 -0700

    Update index.php

--- api/v2/getmemes/index.php
+++ api/v2/getmemes/index.php
@@ -10,5 +10,7 @@ $random_fox_index = rand(1, $FOX_NUM);
 $image_path = 'https://s.wuffs.net/api'.$random_fox_index;
 
 #$data = ['image' => $image_path, 'link' => $link];
-$data = ['image' => $image_path];
+$data = ['image' => $image_path,
+        'TXT' => 'Please use JSON decoder for api',
+        'Version' => '2022.0.4.76'];
 echo json_encode($data);
