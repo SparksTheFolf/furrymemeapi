afc46429ddeae9dab3bb3f0f6d58c6d22b2858ea
Nolan <officialstackemup@gmail.com>
2022-10-18 09:00:33 -0700

    Update index.php

--- api/v2/index.php
+++ api/v2/index.php
@@ -4,6 +4,7 @@ header('Content-Type: application/json');
 
 $FOX_NUM = 32;
 $random_fox_index = rand(1, $FOX_NUM);
+$version = '2022.0.5.1';
 
 $image_path = 'https://s.wuffs.net/api'.$random_fox_index;
 $qr = 'https://s.wuffs.net/api'.$random_fox_index.'.qr';
@@ -13,5 +14,5 @@ $data = ['image' => $image_path,
         'rawLink' => $link,
         'qrCode' => $qr,
         'TXT' => 'Please use JSON decoder for api',
-        'Version' => '2022.0.4.76'];
+        'Version' => $version];
 echo json_encode($data);
