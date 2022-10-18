e846d00669e5f17aae49e4fad0967910f73936f0
Nolan <officialstackemup@gmail.com>
2022-10-18 08:58:41 -0700

    Update index.php

--- api/v2/index.php
+++ api/v2/index.php
@@ -7,8 +7,10 @@ $random_fox_index = rand(1, $FOX_NUM);
 
 $image_path = 'https://s.wuffs.net/api'.$random_fox_index;
 $qr = 'https://s.wuffs.net/api'.$random_fox_index.'.qr';
+$link = 'https://github.com/SparksTheFolf/apiphotos/blob/Master/'.$random_fox_index.'.jpg';
 
 $data = ['image' => $image_path,
+        'rawLink' => $link,
         'qrCode' => $qr,
         'TXT' => 'Please use JSON decoder for api',
         'Version' => '2022.0.4.76'];
