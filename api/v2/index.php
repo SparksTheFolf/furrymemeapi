fa41f46fae72843499e01c69a870899239e7d0b2
Nolan <officialstackemup@gmail.com>
2022-11-02 11:00:05 -0700

    Update index.php

--- api/v2/index.php
+++ api/v2/index.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 $FOX_NUM = 32;
 $random_fox_index = rand(1, $FOX_NUM);
