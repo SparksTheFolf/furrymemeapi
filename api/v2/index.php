9bff71d81c539cb7e76a00d1d621907f42a990b3
Nolan <officialstackemup@gmail.com>
2022-11-02 10:31:34 -0700

    Update index.php

--- api/v2/index.php
+++ api/v2/index.php
@@ -18,4 +18,19 @@ $data = ['image' => $image_path,
         'qrCode' => $qr,
         'about' => $pagecontents,
         'Version' => $version];
-echo json_encode($data);
+
+$json = json_encode(array(
+    "client" => array(
+        "build" => "1.0",
+        "name" => "xxxxxx",
+        "version" => "1.0"
+    ),
+    "protocolVersion" => 4,
+    "data" => array(
+        "distributorId" => "xxxx",
+        "distributorPin" => "xxxx",
+        "locale" => "en-US"
+    )
+));
+
+echo $json;
