ba874c4b2b35af519aae052d3c341481cfccc6f9
Nolan <officialstackemup@gmail.com>
2022-11-02 10:25:37 -0700

    Update index.php

--- api/v2/index.php
+++ api/v2/index.php
@@ -9,7 +9,7 @@ $version = '2022.0.5.1';
 $image_path = 'https://s.wuffs.net/api'.$random_fox_index;
 $qr = 'https://s.wuffs.net/api'.$random_fox_index.'.qr';
 $link = 'https://github.com/SparksTheFolf/apiphotos/blob/Master/'.$random_fox_index.'.jpg?raw=true';
-$pagecontents = file_get_contents("https://raw.githubusercontent.com/SparksTheFolf/apiphotos/Master/txt/13.txt");
+$pagecontents = file_get_contents("https://raw.githubusercontent.com/SparksTheFolf/apiphotos/Master/txt/'.$random_fox_index.'.txt");
 
 
 $data = ['image' => $image_path,
