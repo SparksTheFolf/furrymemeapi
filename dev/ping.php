2c4f04210809a63f60fdea91394dc9c33fbbc2c9
Nolan <officialstackemup@gmail.com>
2022-11-02 10:34:58 -0700

    Create ping.php

---
+++ dev/ping.php
@@ -0,0 +1,29 @@
+<?php
+function ping($host, $timeout = 10)
+{
+    $output = array();
+    $com = 'ping -n -w ' . $timeout . ' -c 1 ' . escapeshellarg($host);
+    $exitcode = 0;
+    exec($com, $output, $exitcode);
+    if ($exitcode == 0 || $exitcode == 1)
+    {
+        foreach($output as $cline)
+        {
+            if (strpos($cline, ' bytes from ') !== FALSE)
+            {
+                $out = (int)ceil(floatval(substr($cline, strpos($cline, 'time=') + 5)));
+                return $out;
+            }
+        }
+    }
+
+    return FALSE;
+}
+
+$total = 0;
+for ($i = 0; $i<=9; $i++)
+{
+    $total += ping('www.google.com');
+}
+
+echo $total/10;
