16455977e7bdddfcc23c673e2b09e2ef9363efa3
Nolan <officialstackemup@gmail.com>
2022-11-03 15:04:53 -0700

    Update captcha.php

--- api/captcha.php
+++ api/captcha.php
@@ -1,8 +1,18 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+
 $captchaGen = rand(0, 8);
 
 $json = json_encode(array(
+    "client" => array(
         'captcha' => $captchaGen
+    ),
+    "server" => array(
+        'dataServ' => date('Y-m-d H:i:s')
+    )
 ));
 
 echo $json;
