f61d1ab65d0928472d6ce6fd833960f3559587e5
Nolan <officialstackemup@gmail.com>
2022-11-04 10:19:00 -0700

    hi

--- api/v2/index.php
+++ api/v2/index.php
@@ -4,8 +4,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-$FOX_NUM = 32;
-
+global/*$FOX_NUM*/ $FOX_NUM;
 
 if($_GET['q'] == ''){
 
