62bbe2a5f3e5953b6f254a1bc1cbe78a416a2664
Nolan <officialstackemup@gmail.com>
2022-11-04 12:02:05 -0700

    Update index.php

--- api/v2/index.php
+++ api/v2/index.php
@@ -1,8 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 $FOX_NUM = 32;
 $notFound = http_response_code(521);
@@ -29,7 +27,7 @@ if($_GET['q'] == ''){
     $qr = 'https://s.wuffs.net/api'.$random_fox_index.'.qr';
     $link = 'https://github.com/SparksTheFolf/apiphotos/blob/Master/'.$random_fox_index.'.jpg?raw=true';
     $pagecontents = file_get_contents('https://raw.githubusercontent.com/SparksTheFolf/apiphotos/Master/txt/'.$random_fox_index.'.txt');
-    
+
     $json = json_encode(array(
         "client" => array(
             'rawLink' => $link,
