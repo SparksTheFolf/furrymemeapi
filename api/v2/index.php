fe1f9efd7c82e102ce9aae932db88cc2a825b13a
Nolan <officialstackemup@gmail.com>
2022-11-04 09:38:46 -0700

    test

--- api/v2/index.php
+++ api/v2/index.php
@@ -5,9 +5,31 @@ header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 
-if ($_GET['q'] ==  '.jpg'){
-    echo 'hi';
-}else{
+if ($_GET['q'] == is_numeric($_GET['q'])) {
+    $random_fox_index = $_GET['q'];
+    $serverVersion = '2022.0.5.1';
+    $releaseVersion = '2022.1.0.0';
+
+
+    $image_path = 'https://s.wuffs.net/api'.$random_fox_index;
+    $qr = 'https://s.wuffs.net/api'.$random_fox_index.'.qr';
+    $link = 'https://github.com/SparksTheFolf/apiphotos/blob/Master/'.$random_fox_index.'.jpg?raw=true';
+    $pagecontents = file_get_contents('https://raw.githubusercontent.com/SparksTheFolf/apiphotos/Master/txt/'.$random_fox_index.'.txt'); # or die("NULL /// No about file found");
+
+
+    $json = json_encode(array(
+        "client" => array(
+            'image' => $image_path,
+            'rawLink' => $link,
+            'qrCode' => $qr,
+            'aboutMeme' => $pagecontents
+        ),
+        "data" => array(
+            'dataServ' => 'https://api.wuffs.net/api/data'
+        )
+    ));
+
+    echo $json;}else{
     $FOX_NUM = 32;
     $random_fox_index = rand(1, $FOX_NUM);
     $serverVersion = '2022.0.5.1';
