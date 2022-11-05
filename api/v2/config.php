41f42f850af98057bfb0b2a819737d1453a7afe2
Sparks <sparks@wuffs.net>
2022-11-04 19:09:35 -0700

    Update config.php

--- api/v2/config.php
+++ api/v2/config.php
@@ -12,3 +12,15 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
+<html>
+<head>
+    <title>BOI</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+    <body>
+        WHY R U TRYING TO HACK ME, Plus I'm not even using this anymore, or am I, Plus I can see your IP anyways. :)
+</body>
+</head>
