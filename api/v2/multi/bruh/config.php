a612168020a39cfbe1ff37289f377b2207c5653a
Nolan <officialstackemup@gmail.com>
2022-11-15 14:15:27 -0700

    Update config.php

--- api/v2/multi/bruh/config.php
+++ api/v2/multi/bruh/config.php
@@ -8,7 +8,7 @@ $dotenv->load();
 $DB_SERVER = getenv('DBSERVER');
 $DB_USERNAME = getenv('DBUSERNAME');
 $DB_PASSWORD = getenv('DBPASSWORD');
-$DB_NAME = getenv('DBUSERNAME');
+$DB_NAME = getenv('DBNAME');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
